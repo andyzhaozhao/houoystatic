1a71f95fa2d70a41de34d22f0ff98a76b171ed2a
andyzhaozhao <andy_zhaozhao@hotmail.com>
2017-06-13 15:54:39 +0800

    Add:文章保存

--- jxy.static/jxycms/js/cms.js
+++ jxy.static/jxycms/js/cms.js
@@ -4,56 +4,71 @@
  */
 (function (cms) {
     //定义页面数据模型
-    var url = window.houoy.public.static.cmsContextPath+"/api" ;
-    cms.userModel = window.houoy.public.createPageModel();
-    cms.userModel.setCurrentData({
-        id: null,
-        title: $("#title").val(),
-        subTitle: $("#subTitle").val(),
-        content: $("#content").val()
+    var url = window.houoy.public.static.cmsContextPath + "/api/essay";
+    cms.model = window.houoy.public.createPageModel();
+    cms.model.setCurrentData({
+        pk_essay: null,
+        essay_name: $("#essay_name").val(),
+        essay_subname: $("#essay_subname").val(),
+        essay_content: $("#essay_content").val()
     });
 
     cms.resetCurrentData = function (data) {
-        cms.userModel.getCurrentData().id = data.id;
-        cms.userModel.getCurrentData().title = data.title;
-        cms.userModel.getCurrentData().subTitle = data.subTitle;
-        cms.userModel.getCurrentData().content = data.content;
-        $("#title").val(cms.userModel.getCurrentData().title);
-        $("#subTitle").val(cms.userModel.getCurrentData().subTitle);
-        cms.um.setContent(cms.userModel.getCurrentData().content);
+        cms.model.getCurrentData().pk_essay = data.pk_essay;
+        cms.model.getCurrentData().essay_name = data.essay_name;
+        cms.model.getCurrentData().essay_subname = data.essay_subname;
+        cms.model.getCurrentData().essay_content = data.essay_content;
+        $("#essay_name").val(cms.model.getCurrentData().essay_name);
+        $("#essay_subname").val(cms.model.getCurrentData().essay_subname);
+        debugger;
+        cms.contentSet('<p>用 JS 设置的内容</p>');
+        cms.contentSet(cms.model.getCurrentData().essay_content);
+    };
+
+    cms.initWangEditor = function () {
+        //加载编辑器的容器
+        if (!cms.um) {
+            debugger;
+            cms.um = new window.wangEditor('container');
+            cms.um.create();
+        }
+    };
+
+    cms.contentSet = function (c) {
+       cms.um.$txt.html(s);
+    };
+
+    cms.contentGet = function () {
+        return cms.um.$txt.html();
     };
 
     //定义页面成员方法
     cms.init = function () {
-        //加载编辑器的容器
-        if(!cms.um){
-            debugger;
-            cms.um = new wangEditor('container');
-            cms.um.create();
-        }
-
         //注册事件监听
         $("#addBtn").click(function () {
-            cms.userModel.setModal(window.houoy.public.PageManage.UIModal.CARD);
-            cms.userModel.setUIState(window.houoy.public.PageManage.UIState.CREATE);
+            cms.model.setModal(window.houoy.public.PageManage.UIModal.CARD);
+            cms.model.setUIState(window.houoy.public.PageManage.UIState.CREATE);
+            cms.initWangEditor();
             cms.resetCurrentData({//新增时候当前缓存数据是空
-                id: null,
-                title: null,
-                subTitle: null,
-                content: null
+                pk_essay: null,
+                essay_name: null,
+                essay_subname: null,
+                essay_content: null
             });
         });
 
         $("#editBtn").click(function () {
-            cms.userModel.setModal(window.houoy.public.PageManage.UIModal.CARD);
-            cms.userModel.setUIState(window.houoy.public.PageManage.UIState.CREATE);
+            cms.model.setModal(window.houoy.public.PageManage.UIModal.CARD);
+            cms.model.setUIState(window.houoy.public.PageManage.UIState.CREATE);
+            cms.initWangEditor();
             cms.resetCurrentData(cms.dataTable.getSelectedRows()[0]);//设置当前选中的行
+
         });
 
         $("#deleteBtn").click(function () {
             if (confirm('你确定要删除选择项目吗？')) {
                 cms.delete(function () {
-                    cms.userModel.setModal(window.houoy.public.PageManage.UIModal.LIST);
+                    cms.model.setModal(window.houoy.public.PageManage.UIModal.LIST);
                     cms.refresh();
                 }, function () {
                 });
@@ -62,30 +77,31 @@
 
         $("#saveBtn").click(function () {
             cms.save(function () {
-                cms.userModel.setUIState(window.houoy.public.PageManage.UIState.SEARCH);
+                cms.model.setUIState(window.houoy.public.PageManage.UIState.SEARCH);
             }, function () {
             });
         });
 
         $("#cancelBtn").click(function () {
-            cms.userModel.setUIState(window.houoy.public.PageManage.UIState.SEARCH);
+            cms.model.setUIState(window.houoy.public.PageManage.UIState.SEARCH);
             cms.resetCurrentData({//新增时候当前缓存数据是空
-                id: null,
-                title: null,
-                subTitle: null,
-                content: null
+                pk_essay: null,
+                essay_name: null,
+                essay_subname: null,
+                essay_content: null
             });
         });
 
         $("#toCardBtn").click(function () {
-            cms.userModel.setModal(window.houoy.public.PageManage.UIModal.CARD);
+            cms.model.setModal(window.houoy.public.PageManage.UIModal.CARD);
+            cms.initWangEditor();
             cms.resetCurrentData(cms.dataTable.getSelectedRows()[0]);//设置当前选中的行
         });
 
         $("#toListBtn").click(function () {
-            cms.userModel.setModal(window.houoy.public.PageManage.UIModal.LIST);
-            cms.userModel.setUIState(window.houoy.public.PageManage.UIState.SEARCH);
-            cms.userModel.setSelectState(window.houoy.public.PageManage.DataState.NONE_SELECT);
+            cms.model.setModal(window.houoy.public.PageManage.UIModal.LIST);
+            cms.model.setUIState(window.houoy.public.PageManage.UIState.SEARCH);
+            cms.model.setSelectState(window.houoy.public.PageManage.DataState.NONE_SELECT);
             cms.refresh();
         });
 
@@ -94,106 +110,93 @@
         });
 
         $("#searchResetBtn").click(function () {
-            $("input[name='title']").val("");
-            $("input[name='content']").val("");
+            $("input[name='essay_name']").val("");
+            $("input[name='essay_subname']").val("");
             cms.refresh();
         });
 
         //初始化
-        cms.userModel.setUIState(window.houoy.public.PageManage.UIState.SEARCH);//默认是查询状态
-        cms.userModel.setSelectState(window.houoy.public.PageManage.DataState.NONE_SELECT);//默认是没有选中数据
-        cms.userModel.setModal(window.houoy.public.PageManage.UIModal.LIST);//默认是列表模式
+        cms.model.setUIState(window.houoy.public.PageManage.UIState.SEARCH);//默认是查询状态
+        cms.model.setSelectState(window.houoy.public.PageManage.DataState.NONE_SELECT);//默认是没有选中数据
+        cms.model.setModal(window.houoy.public.PageManage.UIModal.LIST);//默认是列表模式
 
         cms.dataTable = window.houoy.public.createDataTable({
             dataTableID: "table",
-            url: url+"/essay",
+            url: url + "/retrieve",
             param: {//查询参数
-                title: function(){return $("input[name='title']").val()},
-                content: function(){return $("input[name='content']").val()}
+                essay_name: function () {
+                    return $("input[name='essay_name']").val();
+                },
+                essay_subname: function () {
+                    return $("input[name='essay_subname']").val();
+                }
             },
-            columns: [{"title": "序列号", 'data': 'id', "visible": false},
-                {"title": "标题", 'data': 'title'},
-                {"title": "副标题", 'data': 'subTitle'}],
+            columns: [{"title": "序列号", 'data': 'pk_essay', "visible": false},
+                {"title": "标题", 'data': 'essay_name'},
+                {"title": "副标题", 'data': 'essay_subname'}],
             onSelectChange: function (selectedNum, selectedRows) {
                 if (selectedNum > 1) {
-                    cms.userModel.setSelectState(window.houoy.public.PageManage.DataState.MUL_SELECT);
+                    cms.model.setSelectState(window.houoy.public.PageManage.DataState.MUL_SELECT);
                 } else if (selectedNum == 1) {
-                    cms.userModel.setSelectState(window.houoy.public.PageManage.DataState.ONE_SELECT);
+                    cms.model.setSelectState(window.houoy.public.PageManage.DataState.ONE_SELECT);
                 } else {
-                    cms.userModel.setSelectState(window.houoy.public.PageManage.DataState.NONE_SELECT);//没有选中数据
+                    cms.model.setSelectState(window.houoy.public.PageManage.DataState.NONE_SELECT);//没有选中数据
                 }
             }
         });
     };
 
     cms.save = function (onSuccess, onError) {
-        if (!($("#title").val()) || !($("#subTitle").val())) {
+        if (!($("#essay_name").val()) || !($("#essay_subname").val())) {
             alert("请填写完整信息");
         } else {
-            cms.userModel.getCurrentData().title = $("#title").val();
-            cms.userModel.getCurrentData().subTitle = $("#subTitle").val();
-            cms.userModel.getCurrentData().content =   cms.um.getContent();
+            cms.model.getCurrentData().essay_name = $("#essay_name").val();
+            cms.model.getCurrentData().essay_subname = $("#essay_subname").val();
+            cms.model.getCurrentData().essay_content = cms.contentGet();
 
-            $.ajax({
-                type: 'post',
-                url: url + '/essay',
-                contentType: "application/json;charset=UTF-8",
-                dataType: "json",
-                data: JSON.stringify(cms.userModel.getCurrentData()),
-                success: function (data) {
-                    if (data.success) {
-                        alert("保存成功");
-                        onSuccess();
-                    } else {
-                        alert("保存失败:" + data.msg);
-                    }
-                },
-                error: function (data) {
-                    alert("保存失败！"+data);
-                    onError();
+            window.houoy.public.post(url + '/save', JSON.stringify(cms.model.getCurrentData()), function (data) {
+                if (data.success) {
+                    alert("保存成功");
+                    cms.model.setUIState(window.houoy.public.PageManage.UIState.SEARCH);
+                } else {
+                    alert("保存失败:" + data.msg);
                 }
+            }, function (err) {
+                alert("请求保存失败！");
             });
         }
     };
 
     cms.delete = function (onSuccess, onError) {
         var _ids = [];
-        switch (cms.userModel.getModal()) {
+        switch (cms.model.getModal()) {
             case window.houoy.public.PageManage.UIModal.CARD:
-                _ids[0] = cms.userModel.getCurrentData().id;
+                _ids[0] = cms.model.getCurrentData().pk_essay;
                 break;
             case window.houoy.public.PageManage.UIModal.LIST:
                 $.each(cms.dataTable.getSelectedRows(), function (index, value) {
-                    _ids[index] = value.id;
+                    _ids[index] = value.pk_essay;
                 });
                 break;
             default:
                 break;
         }
 
-        $.ajax({
-            type: 'delete',
-            url: url + '/delete',
-            contentType: "application/json;charset=UTF-8",
-            dataType: "json",
-            data: JSON.stringify(_ids),
-            success: function (data) {
-                if (data.success) {
-                    onSuccess();
-                } else {
-                    alert("删除失败:" + data);
-                }
-            },
-            error: function (data) {
-                alert("删除失败！");
-                onError();
+        window.houoy.public.post(url + '/delete', JSON.stringify(_ids), function (data) {
+            if (data.success) {
+                cms.model.setModal(window.houoy.public.PageManage.UIModal.LIST);
+                cms.refresh();
+            } else {
+                alert("删除失败:" + data);
             }
+        }, function (err) {
+            alert("请求删除失败！");
         });
     };
 
     cms.refresh = function () {
         cms.dataTable.refresh();
-    }
+    };
 
     cms.init();
 })(window.houoy.cms || {});
