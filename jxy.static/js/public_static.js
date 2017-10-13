284806fc75527f48cfd539fd67461de267d864e0
andyzhaozhao <andy_zhaozhao@hotmail.com>
2017-10-13 11:51:16 +0800

    Mod:change path

--- jxy.static/js/public_static.js
+++ jxy.static/js/public_static.js
@@ -16,8 +16,10 @@
     //定义全局变量
     if (typeof s.static == "undefined") {
         s.static = {
-            contextPath: "http://localhost:8889",//后端服务器地址
-            cmsContextPath: "http://localhost:8888",//后端服务器地址
+            contextPath: "http://182.92.128.240:8889",//后端服务器地址
+            cmsContextPath: "http://182.92.128.240:8888",//后端服务器地址
+            // contextPath: "http://localhost:8889",//后端服务器地址
+            //cmsContextPath: "http://localhost:8888",//后端服务器地址
             xauthtoken: "com.jiaxiaoyi.xauthtoken",//sessionid的token
             getURLParameter: function (name) {//获得页面url参数
                 return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null;
